aef37e570e3c4eec6b9a5f298c4cb898fc5918a9
hasan13222 <jamil8305@gmail.com>
2024-04-10 18:06:30 +0600

    json file dir changed

--- src/utils/getAgents.js
+++ src/utils/getAgents.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
 export const getAgents = async () => {
-    const res = await fs.readFile(process.cwd() + '/public/agents.json');
+    const res = await fs.readFile(process.cwd() + '/agents.json');
     return JSON.parse(res);
 }
--- src/utils/getHomes.js
+++ src/utils/getHomes.js
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 
 export const getHomes = async () => {
-    const res = await fs.readFile(process.cwd() + '/public/homes.json');
+    const res = await fs.readFile(process.cwd() + '/homes.json');
     const result = JSON.parse(res);
     return result;
 }
